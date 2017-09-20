b972f657615f233e6cde6544eb830465ca1bc920
Gilolume <g.guillaume91350@gmail.com>
2017-09-20 16:30:26 +0200

    Add file reader

--- app/src/main/java/com/example/guill/myapplication/MainActivity.java
+++ app/src/main/java/com/example/guill/myapplication/MainActivity.java
@@ -3,18 +3,31 @@ package com.example.guill.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//Import Components
+import android.app.Activity;
+import android.net.Uri;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.EditText;
-import android.widget.Toast;
-//End import components
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editTextSelectFile;
-    Button buttonStart;
+    private static final int ACTIVITY_CHOOSE_FILE = 3;
+    private static final int CREATE_REQUEST_CODE = 40;
+    private static final int OPEN_REQUEST_CODE = 41;
+    private static final int SAVE_REQUEST_CODE = 42;
+
+    Button buttonChooseFile;
+    TextView textViewResult;
 
 
     @Override
@@ -22,34 +35,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextSelectFile = (EditText) findViewById(R.id.editTextSelectFile);
-        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonChooseFile = (Button) findViewById(R.id.buttonChooseFile);
+        textViewResult = (TextView) findViewById(R.id.textViewResult);
 
         //Click listener
-        editTextSelectFile.setOnClickListener(new View.OnClickListener() {
+        buttonChooseFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("text/plain");
-                startActivityForResult(intent, 7);
+                Intent chooseFile;
+                Intent intent;
+                chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+                chooseFile.setType("*/*");
+                intent = Intent.createChooser(chooseFile, "Choose a file");
+                startActivityForResult(intent, OPEN_REQUEST_CODE);
+
             }
         });
         //End click listener
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
+    protected void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        Uri currentUri = null;
 
-        switch (requestCode) {
-            case 7:
-                if (resultCode == RESULT_OK) {
-                    String PathHolder = data.getData().getPath();
-                    Toast.makeText(MainActivity.this, PathHolder, Toast.LENGTH_LONG).show();
-                    editTextSelectFile.setText(PathHolder);
+        if (resultCode == Activity.RESULT_OK)
+        {
+            if (requestCode == OPEN_REQUEST_CODE)
+            {
+
+                if (resultData != null) {
+                    currentUri = resultData.getData();
+
+                    try {
+                        String content =
+                                readFileContent(currentUri);
+                        textViewResult.setText(content);
+                    } catch (IOException e) {
+                        // Handle error here
+                    }
                 }
-                break;
+            }
         }
+    }
+
+
+    private String readFileContent(Uri uri) throws IOException {
+
+        InputStream inputStream = getContentResolver().openInputStream(uri);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder stringBuilder = new StringBuilder();
+        String currentline;
+        while ((currentline = reader.readLine()) != null) {
+            stringBuilder.append(currentline + "\n");
+        }
+        inputStream.close();
+        return stringBuilder.toString();
     }
 
 }
